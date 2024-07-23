678a592f177c43d892c9ffcb6fadfd02a9d9c24e
SalmaSyawalan <salma.syawalan@gmail.com>
2024-07-23 16:49:27 +0700

    add props and change the web theme

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = ({ blogs, title }) => {
+    // const blogs = props.blogs;
+
+    return ( 
+        <div className="blog-list">
+            <h2 className="sub-header">{title}</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Author: {blog.author}</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,30 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [name, setName] = useState("Mario")
-    const [age, setAge] = useState(30)
+    // usage example of useState
+    // const [name, setName] = useState("Mario")
+    // const [age, setAge] = useState(30)
+
+    const [blogs, setBlogs] = useState([
+        { title: 'White Core Phylosophy', body: 'lorem ipsum', author: 'alius', id: 1},
+        { title: 'Solidify the Stardust to Use It as Weapon', body: 'lorem ipsum', author: 'nebula', id: 2},
+        { title: 'Adjust the Range of Extreme Gravity', body: 'lorem ipsum', author: 'quasar', id: 3},
+    ])
 
     const handleClick = () => {
-        setName('Luigi');
-        setAge(age + 1);
+        // setName('Luigi');
+        // setAge(age + 1);
     }
 
     return ( 
         <div className="home">
-            <h2>Home page</h2>
+            {/* <h2>Home page</h2> */}
+            <BlogList blogs={blogs} title='All Blogs'/>
+            <BlogList blogs={blogs.filter((blog) => blog.author == 'alius')} title='Selected Blogs'/>
+            {/* usage example
             <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>click</button>
+            <button onClick={handleClick}>click</button> */}
         </div>
      );
 }
